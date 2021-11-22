719cdbb9eafa0f171fd354108705bb52af4197e5
shoiraTa <shoira.shakirovna@gmail.com>
2021-11-22 21:51:38 +0500

    Adding components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate.js';
+import Card from './Card';
+
+function ExpenseItem(props) {
+  return (
+    <Card className = "expense-item">
+      <ExpenseDate expenseDate = {props.expenseDate}/>
+      <div className = "expense-item__description">
+        <h2>{props.expenseTitle}</h2>
+        <div className="expense-item__price">${props.expensePrice}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
---
+++ src/components/Expenses.js
@@ -0,0 +1,49 @@
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+import Card from './Card';
+
+function Expenses() {
+  const expenses = [
+    {
+      expenseDate: new Date(2021, 2, 12),
+      expensePrice: 268,
+      expenseTitle: 'Car ensurance',
+    },
+    {
+      expenseDate: new Date(2021, 2, 12),
+      expensePrice: 110,
+      expenseTitle: 'Dinner',
+    },
+    {
+      expenseDate: new Date(2021, 2, 12),
+      expensePrice: 52,
+      expenseTitle: 'Cheese',
+    },
+    {
+      expenseDate: new Date(2021, 2, 12),
+      expensePrice: 8,
+      expenseTitle: 'Toilet paper',
+    },
+  ];
+  return (
+    <Card className= "expenses-container">
+      <ExpenseItem
+        expenseDate={expenses[0].expenseDate}
+        expensePrice={expenses[0].expensePrice}
+        expenseTitle={expenses[0].expenseTitle}
+     />
+     <ExpenseItem
+        expenseDate={expenses[1].expenseDate}
+        expensePrice={expenses[1].expensePrice}
+        expenseTitle={expenses[1].expenseTitle}
+     />
+     <ExpenseItem
+        expenseDate={expenses[2].expenseDate}
+        expensePrice={expenses[2].expensePrice}
+        expenseTitle={expenses[2].expenseTitle}
+     />
+    </Card>
+  );
+}
+
+export default Expenses;
